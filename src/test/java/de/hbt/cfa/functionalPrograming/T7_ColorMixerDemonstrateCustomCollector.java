4cbd7c83bbadb5a3233b11be21aa0abbdaa5b4bc
sku <sku@hbt.de>
2023-08-23 22:35:20 +0200

    improve example so that it uses random colors

--- src/test/java/de/hbt/cfa/functionalPrograming/T7_ColorMixerDemonstrateCustomCollector.java
+++ src/test/java/de/hbt/cfa/functionalPrograming/T7_ColorMixerDemonstrateCustomCollector.java
@@ -1,17 +1,21 @@
 package de.hbt.cfa.functionalPrograming;
 
-import java.awt.*;
-import java.util.List;
+import java.util.stream.IntStream;
 
 public class T7_ColorMixerDemonstrateCustomCollector {
 
     public static void main(String[] args) {
-        List<Integer> colors = List.of(Color.BLUE.getRGB(), Color.RED.getRGB(), Color.GREEN.getRGB(), Color.YELLOW.getRGB(), Color.CYAN.getRGB(), Color.MAGENTA.getRGB(), Color.ORANGE.getRGB(), Color.PINK.getRGB(), Color.WHITE.getRGB(), Color.BLACK.getRGB());
-        final ColorMixer colorMixer = colors.stream()
+        // mix a list of random colors but never manifest the list of colors
+        final ColorMixer colorMixer = IntStream.generate(T7_ColorMixerDemonstrateCustomCollector::randomColor)
+                .limit(10000)
                 .parallel()
                 .collect(ColorMixer::new, ColorMixer::accumulate, ColorMixer::combine);
         System.out.printf("I mixed a total of %s colors and now it's" +
-                "\033[38;2;%02.0f;%02.0f;%02.0fm %s \n", colorMixer.totalAmount, colorMixer.r, colorMixer.g, colorMixer.b, colorMixer.toString());
+                "\033[38;2;%02.0f;%02.0f;%02.0fm %s%n", colorMixer.totalAmount, colorMixer.r, colorMixer.g, colorMixer.b, colorMixer.toString());
+    }
+
+    private static int randomColor() {
+        return (int) (Math.random() * 0xffffff);
     }
 
     private static int r(int hex) {
@@ -35,31 +39,35 @@ public class T7_ColorMixerDemonstrateCustomCollector {
 
         @Override
         public String toString() {
-            return String.format("mix (totalAmount=%d,r=%02.0f,g=%02.0f,b=%02.0f)", totalAmount, r, g, b);
+            if (totalAmount == 0) return "ColorMix[empty]";
+            return String.format("\033[38;2;%02.0f;%02.0f;%02.0fmColorMix[totalAmount=%d,r=%02.0f,g=%02.0f,b=%02.0f]", r, g, b, totalAmount, r, g, b);
         }
 
         public static void accumulate(ColorMixer colorMixer, int color) {
             if (colorMixer.totalAmount == 0) {
-                System.out.printf("Starting with color %s\n", Integer.toHexString(color));
                 colorMixer.r = r(color);
                 colorMixer.g = g(color);
                 colorMixer.b = b(color);
                 colorMixer.totalAmount = 1;
+                System.out.printf("\033[38;2;%02.0f;%02.0f;%02.0fmStarting with color %s%n", colorMixer.r, colorMixer.g, colorMixer.b, Integer.toHexString(color));
             } else {
-                System.out.printf("Adding color %s\n", Integer.toHexString(color));
+                System.out.printf("\033[38;2;%02d;%02d;%02dmAdding color %s", r(color), g(color), b(color), Integer.toHexString(color));
                 colorMixer.r = (colorMixer.r * colorMixer.totalAmount + r(color)) / (colorMixer.totalAmount + 1);
                 colorMixer.g = (colorMixer.g * colorMixer.totalAmount + g(color)) / (colorMixer.totalAmount + 1);
                 colorMixer.b = (colorMixer.b * colorMixer.totalAmount + b(color)) / (colorMixer.totalAmount + 1);
                 colorMixer.totalAmount++;
+                System.out.printf(" and got %s%n", colorMixer);
+
             }
         }
 
         public static void combine(ColorMixer colorMixer, ColorMixer colorMixer2) {
-            System.out.println("Combining " + colorMixer + " with " + colorMixer2);
+            System.out.printf("Combining %s with %s", colorMixer, colorMixer2);
             colorMixer.r = ((colorMixer.r * colorMixer.totalAmount) + (colorMixer2.r * colorMixer2.totalAmount)) / (colorMixer.totalAmount + colorMixer2.totalAmount);
             colorMixer.g = ((colorMixer.g * colorMixer.totalAmount) + (colorMixer2.g * colorMixer2.totalAmount)) / (colorMixer.totalAmount + colorMixer2.totalAmount);
             colorMixer.b = ((colorMixer.b * colorMixer.totalAmount) + (colorMixer2.b * colorMixer2.totalAmount)) / (colorMixer.totalAmount + colorMixer2.totalAmount);
             colorMixer.totalAmount += colorMixer2.totalAmount;
+            System.out.printf(" and got %s%n", colorMixer);
         }
     }
 }
